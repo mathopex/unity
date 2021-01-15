5dc10673fd6184bc0fee5c8eb1e30eb8eb4ed9fc
mathopex <56123787+mathopex@users.noreply.github.com>
2021-01-15 14:48:37 +0100

    update 15/01/2021

---
+++ Construction Tycoon/Assets/CityBuilder/Script/CellScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CellScript : MonoBehaviour
+{
+    GameManager gm;
+    void Start()
+    {
+        gm = GameObject.Find("Grid").GetComponent<GameManager>();
+    }
+
+    // Update is called once per frame
+   
+    private void OnMouseEnter()
+    {
+        GetComponent<Renderer>().material.color = Color.green;
+    }
+
+
+    private void OnMouseExit()
+    {
+        GetComponent<Renderer>().material.color = Color.white;
+    }
+
+    private void OnMouseUp()
+    {
+
+        GameObject go = Instantiate(gm.building, transform.position, Quaternion.Euler(-90,0,0));
+        go.name = "Building_" + gameObject.name;
+    } 
+
+}
+
+   
+
+
+
