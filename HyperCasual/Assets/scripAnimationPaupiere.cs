c6c98273677ff6f5954468e60a4e5a89ea569b2a
mathopex <56123787+mathopex@users.noreply.github.com>
2021-08-13 17:30:41 +0200

    update 13/08/2021

---
+++ HyperCasual/Assets/scripAnimationPaupiere.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scripAnimationPaupiere : MonoBehaviour
+{
+    public SpriteRenderer paupierInfGauche;
+    public SpriteRenderer paupierInfDroite;
+  
+
+   public void ActrivePaupiere()
+    {
+        paupierInfDroite.enabled = true;
+        paupierInfGauche.enabled = true;
+    }
+
+
+    public void desactivepaupiere()
+    {
+        paupierInfDroite.enabled = false;
+        paupierInfGauche.enabled = false;
+    }
+
+    
+}
